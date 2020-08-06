65fe40fb185b25082af1cd9466b0639e8fb86a42
Kniazev Aleksandr <a.kniazev@team-pravo.ru>
2020-08-06 10:29:20 +0500

    step

---
+++ app/Denis/migrations/2020_08_05_191346_parent_schedule_id_nullable.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ParentScheduleIdNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \DB::sta('ALTER TABLE bot_conversations ALTER COLUMN parent_schedule_id DROP NOT NULL;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
